c25e22f6ffd3d272d876c86505345352eb8c2df6
BlakeAlmon <all.business.blake@gmail.com>
2023-10-17 18:43:19 -0700

    CSV->JSON
    -Blake

--- Bob_all_in_one_withoutRAG_timed_double_quotes.py
+++ Bob_all_in_one_withoutRAG_timed_double_quotes.py
@@ -13,6 +13,9 @@ from urllib.parse import quote
 
 load_dotenv()
 
+os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
+
+
 # Reddit Section
 def get_reddit_data(num_posts):
     clientSecretKey = os.getenv("CLIENT_SECRET_KEY")
@@ -30,8 +33,7 @@ def get_reddit_data(num_posts):
             script_or_style.decompose()
         text = ' '.join(soup.stripped_strings)
         posts.append({'title': post.title, 'url': post.url, 'text': text})
-
-    return posts
+	
 
 # NewsAPI Section
 def get_news_data(query, num_articles):
@@ -90,5 +92,93 @@ def main():
         print(json_string.encode('utf-8'))  # This line will print the JSON to the console
         f.write(json_string)
 
+
 if __name__ == "__main__":
     main()
+
+
+
+#Section I added for JSON -> CSV
+
+
+now = datetime.datetime.now()
+timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
+
+ 
+import json 
+
+
+def read_json(filename: str) -> dict: 
+
+	try: 
+		with open(filename, "r") as f: 
+			data = json.loads(f.read()) 
+	except: 
+		raise Exception(f"Reading {filename} file encountered an error") 
+
+	return data 
+
+
+def normalize_json(data: dict) -> dict: 
+
+	new_data = dict() 
+	for key, value in data.items(): 
+		if not isinstance(value, dict): 
+			new_data[key] = value 
+		else: 
+			for k, v in value.items(): 
+				new_data[key + "_" + k] = v 
+
+	return new_data 
+
+
+def generate_csv_data(data: dict) -> str: 
+
+	# Defining CSV columns in a list to maintain 
+	# the order 
+	csv_columns = data.keys() 
+
+	# Generate the first row of CSV 
+	csv_data = ",".join(csv_columns) + "\n"
+
+	# Generate the single record present 
+	new_row = list() 
+	for col in csv_columns: 
+		new_row.append(str(data[col])) 
+
+	# Concatenate the record with the column information 
+	# in CSV format 
+	csv_data += ",".join(new_row) + "\n"
+
+	return csv_data 
+
+
+def write_to_file(data: str, filepath: str) -> bool: 
+
+	try: 
+		with open(filepath, "w+") as f: 
+			f.write(data) 
+	except: 
+		raise Exception(f"Saving data to {filepath} encountered an error") 
+
+
+def main(): 
+	# Read the JSON file as python dictionary 
+	data = read_json(filename="all_data_" + timestamp + ".json") 
+
+	# Normalize the nested python dict 
+	new_data = normalize_json(data=data) 
+
+	# Pretty print the new dict object 
+	print("New dict:", new_data) 
+
+	# Generate the desired CSV data 
+	csv_data = generate_csv_data(data=new_data) 
+
+	# Save the generated CSV data to a CSV file 
+	write_to_file(data=csv_data, filepath="data.csv") 
+
+
+if __name__ == '__main__': 
+	main() 
+
