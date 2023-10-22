1cfa3cea1235e39ac8d416fb64b1d95b15db5c8f
BlakeAlmon <all.business.blake@gmail.com>
2023-10-21 18:22:31 -0700

    SummarizationforReddit/Youtube
    This sometimes hallucinates.

--- Bob_all_in_one_withoutRAG_timed_double_quotes.py
+++ Bob_all_in_one_withoutRAG_timed_double_quotes.py
@@ -10,16 +10,22 @@ from youtube_transcript_api import YouTubeTranscriptApi
 from pytube import YouTube
 from dotenv import load_dotenv
 from urllib.parse import quote
+import time
+
+#Summarization
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import TextLoader
 
 load_dotenv()
 
 os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
 
 
+
 # Reddit Section
 def get_reddit_data(num_posts):
     clientSecretKey = os.getenv("CLIENT_SECRET_KEY")
-    reddit = praw.Reddit(client_id="RqbArZ6NcN2TVKR6MaPY1w",
+    reddit = praw.Reddit(client_id="kMolVsEMMe0041y37FnL_Q",
                          client_secret=clientSecretKey,
                          user_agent="Scraper")
     subreddit = reddit.subreddit("technews")
@@ -32,8 +38,15 @@ def get_reddit_data(num_posts):
         for script_or_style in soup(["script", "style"]):
             script_or_style.decompose()
         text = ' '.join(soup.stripped_strings)
+        
         posts.append({'title': post.title, 'url': post.url, 'text': text})
-	
+    
+    return posts
+    
+    
+        
+        
+    
 
 # NewsAPI Section
 def get_news_data(query, num_articles):
@@ -68,11 +81,74 @@ def get_youtube_data(query, max_results):
 
     return videos
 
+# JSON -> CSV  Section
+def read_json(filename: str) -> dict: 
+
+    try: 
+        with open(filename, "r") as f: 
+            data = json.loads(f.read()) 
+    except: 
+        raise Exception(f"Reading {filename} file encountered an error") 
+
+    return data 
+
+
+def normalize_json(data: dict) -> dict: 
+
+    new_data = dict() 
+    for key, value in data.items(): 
+        if not isinstance(value, dict): 
+            new_data[key] = value 
+        else: 
+            for k, v in value.items(): 
+                new_data[key + "_" + k] = v 
+
+    return new_data 
+
+
+def generate_csv_data(data: dict) -> str: 
+
+    # Defining CSV columns in a list to maintain 
+    # the order 
+    csv_columns = data.keys() 
+
+    # Generate the first row of CSV 
+    csv_data = ",".join(csv_columns) + "\n"
+
+    # Generate the single record present 
+    new_row = list() 
+    for col in csv_columns: 
+        new_row.append(str(data[col])) 
+
+    # Concatenate the record with the column information 
+    # in CSV format 
+    csv_data += ",".join(new_row) + "\n"
+
+    return csv_data 
+
+
+def write_to_file(data: str, filepath: str) -> bool: 
+
+    try: 
+        with open(filepath, "w+") as f: 
+            f.write(data) 
+    except: 
+        raise Exception(f"Saving data to {filepath} encountered an error") 
+
+
+
+
+
+
 # Main function to get data from all sources and save to a JSON file
 def main():
-    reddit_data = get_reddit_data(3)
-    news_data = get_news_data('artificial intelligence', 5)
-    youtube_data = get_youtube_data('tech news', 5)
+    numRedditPosts = 3
+    numNewsPosts = 5
+    numYoutubePosts = 5
+
+    reddit_data = get_reddit_data(numRedditPosts)
+    news_data = get_news_data('artificial intelligence', numNewsPosts)
+    youtube_data = get_youtube_data('tech news', numYoutubePosts)
 #edit the number to modify the number of articles in the json file
     all_data = {
         'reddit': reddit_data,
@@ -89,8 +165,129 @@ def main():
     
     with open(filename, 'w', encoding='utf-8') as f:
         json_string = json.dumps(all_data, ensure_ascii=False, indent=4)
-        print(json_string.encode('utf-8'))  # This line will print the JSON to the console
         f.write(json_string)
+        print(json_string.encode('utf-8'))  # This line will print the JSON to the console
+    
+
+
+    #Opening created all data json file
+    f = open(filename)
+    allDataFile = json.load(f)
+
+    #Query for GPT-4
+    query = "Summarize the text based of the title into 10 concise key points."
+
+   
+    
+    
+    #Reddit Summarization
+    for x in range(3):
+        
+        #Putting data into txt file to be summarized
+        with open("summarizedData.txt", "w") as f:
+            f.write('title : ' + repr(allDataFile["reddit"][x]["title"]) + '\n' )
+            f.write(repr(allDataFile["reddit"][x]["text"]) + '\n' )
+        
+        
+
+        #loading data into usable data for summarization
+        loader = TextLoader('summarizedData.txt')
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+
+        #allDataFile['reddit'][x]['text'] = index.query(query)
+        summarizedData = index.query(query) 
+
+        # edit data
+        allDataFile["reddit"][x]["summarizedText"] = summarizedData
+
+        # overwrite file
+        with open(filename, 'w') as f:
+            json.dump(allDataFile, f, indent=4)
+
+
+        #deleting everything within the summarized file
+        with open("summarizedData.txt", "w") as f:
+            f.truncate(0)
+     
+
+    #------ UNABLE TO SUMMARIZE FOR NOW -----
+    #News Summarization
+    """ for x in range(5):
+               
+        with open("summarizedData.txt", "w") as f:
+            f.write('title : ' + repr(allDataFile["news"][x]["title"]) + '\n' )
+            f.write(repr(allDataFile["news"][x]["text"]) + '\n' )
+        
+        
+
+
+        loader = TextLoader('summarizedData.txt')
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+
+        #allDataFile['reddit'][x]['text'] = index.query(query)
+        summarizedData = index.query(query) 
+
+        # edit data
+        allDataFile["news"][x]["summarizedText"] = summarizedData
+
+        # overwrite file
+        with open(filename, 'w') as f:
+            json.dump(allDataFile, f, indent=4)
+
+        with open("summarizedData.txt", "w") as f:
+            f.truncate(0) """
+        
+
+   #Youtube Summarization
+
+    query1 = "Summarize the data into 10 sentences. DO not include information about the narrator. make the summarization only based on the text."
+    for x in range(5):
+               
+        with open("summarizedData.txt", "w") as f:
+            f.write(repr(allDataFile["youtube"][x]["text"]) + '\n' )
+        
+        time.sleep(2)
+
+        loader1 = TextLoader('summarizedData.txt')
+        index1 = VectorstoreIndexCreator().from_loaders([loader1])
+
+
+        #allDataFile['reddit'][x]['text'] = index.query(query)
+        summarizedData = index1.query(query1) 
+
+        # edit data
+        allDataFile["youtube"][x]["summarizedText"] = summarizedData
+
+        # overwrite file
+        with open(filename, 'w') as f:
+            json.dump(allDataFile, f, indent=4)
+
+        with open("summarizedData.txt", "w") as f:
+            f.truncate(0)   
+
+
+
+    # JSON -> CSV SECTION
+
+
+   # Read the JSON file as python dictionary 
+    data = read_json(filename="all_data_" + timestamp + ".json") 
+
+    # Normalize the nested python dict 
+    new_data = normalize_json(data=data) 
+
+    # Pretty print the new dict object 
+    print("New dict:", new_data) 
+
+    # Generate the desired CSV data 
+    csv_data = generate_csv_data(data=new_data) 
+
+    # Save the generated CSV data to a CSV file 
+    write_to_file(data=csv_data, filepath="data.csv") 
+
+        
 
 
 if __name__ == "__main__":
@@ -98,87 +295,8 @@ if __name__ == "__main__":
 
 
 
-#Section I added for JSON -> CSV
 
 
-now = datetime.datetime.now()
-timestamp = now.strftime("%Y-%m-%d_%H-%M-%S")
-
- 
-import json 
 
 
-def read_json(filename: str) -> dict: 
-
-	try: 
-		with open(filename, "r") as f: 
-			data = json.loads(f.read()) 
-	except: 
-		raise Exception(f"Reading {filename} file encountered an error") 
-
-	return data 
-
-
-def normalize_json(data: dict) -> dict: 
-
-	new_data = dict() 
-	for key, value in data.items(): 
-		if not isinstance(value, dict): 
-			new_data[key] = value 
-		else: 
-			for k, v in value.items(): 
-				new_data[key + "_" + k] = v 
-
-	return new_data 
-
-
-def generate_csv_data(data: dict) -> str: 
-
-	# Defining CSV columns in a list to maintain 
-	# the order 
-	csv_columns = data.keys() 
-
-	# Generate the first row of CSV 
-	csv_data = ",".join(csv_columns) + "\n"
-
-	# Generate the single record present 
-	new_row = list() 
-	for col in csv_columns: 
-		new_row.append(str(data[col])) 
-
-	# Concatenate the record with the column information 
-	# in CSV format 
-	csv_data += ",".join(new_row) + "\n"
-
-	return csv_data 
-
-
-def write_to_file(data: str, filepath: str) -> bool: 
-
-	try: 
-		with open(filepath, "w+") as f: 
-			f.write(data) 
-	except: 
-		raise Exception(f"Saving data to {filepath} encountered an error") 
-
-
-def main(): 
-	# Read the JSON file as python dictionary 
-	data = read_json(filename="all_data_" + timestamp + ".json") 
-
-	# Normalize the nested python dict 
-	new_data = normalize_json(data=data) 
-
-	# Pretty print the new dict object 
-	print("New dict:", new_data) 
-
-	# Generate the desired CSV data 
-	csv_data = generate_csv_data(data=new_data) 
-
-	# Save the generated CSV data to a CSV file 
-	write_to_file(data=csv_data, filepath="data.csv") 
-
-
-if __name__ == '__main__': 
-	main() 
 
